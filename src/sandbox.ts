2fac6eed4246df6d4f72eccfc6604a948aabbc97
Soanvig <soanvig@gmail.com>
2019-04-11 21:46:07 +0200

    [#1] fix sandbox

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,4 +1,5 @@
 import { TExtendFunction, BinaryPipe } from './BinaryPipe';
+import { iterateBuffer } from './iterateBuffer';
 
 function generator<T> (func: (val: string) => T): TExtendFunction<T> {
   return (buffer, previousValue) => ({
@@ -16,7 +17,7 @@ function generator2<T> (func: (val: number) => T): TExtendFunction<T> {
 
 const extender1 = generator((cur) => ({ test: cur }));
 const extender2 = generator2((cur) => ({ test2: cur }));
-const iterator = bufferGenerator(Buffer.from([1]));
+const iterator = iterateBuffer(Buffer.from([1]));
 
 console.log(BinaryPipe(iterator, { foobar: 'barfoo' }).pipe(extender1)); // { test: 'foobar' }
 console.log(BinaryPipe(iterator).pipe(extender1, extender2)); // { test: 'foobar', test2: 'foobar' }
