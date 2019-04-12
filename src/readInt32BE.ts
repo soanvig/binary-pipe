c65f5847a26d01c471979746f21d8e087afeb31c
Soanvig <soanvig@gmail.com>
2019-04-12 21:12:38 +0200

    [#3] add readInt32BE

---
+++ src/readInt32BE.ts
@@ -0,0 +1,19 @@
+import { TExtendFunction, TFormatter } from './BinaryPipe';
+import { take } from './take';
+
+/**
+ * Read four bytes as BE integer
+ */
+export function readInt32BE<T> (func: (byte: number) => T): TExtendFunction<T>;
+export function readInt32BE<T, U> (func: (byte: U) => T, formatter: TFormatter<U>): TExtendFunction<T>;
+export function readInt32BE<T, U> (func: (byte: number | U) => T, formatter?: TFormatter<U>): TExtendFunction<T> {
+  return (generator, previousValue) => {
+    const buffer: Buffer = take(generator, 4);
+    const int: number = buffer.readInt32BE(0);
+    const formatted = formatter ? formatter([int]) : int;
+    return {
+      ...previousValue,
+      ...func(formatted),
+    };
+  };
+}
