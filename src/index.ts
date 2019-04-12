0aa35de0fb134676bc0ff4a296ebb96786402845
Soanvig <soanvig@gmail.com>
2019-04-12 22:02:15 +0200

    [#3] export read functions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { BinaryPipe } from './BinaryPipe';
-export { bufferGenerator as iterateBuffer } from './bufferGenerator';
+export { bufferGenerator } from './bufferGenerator';
+export { readBytes } from './readBytes';
+export { readInt8 } from './readInt8';
+export { readInt16LE } from './readInt16LE';
+export { readInt16BE } from './readInt16BE';
+export { readInt32LE } from './readInt32LE';
+export { readInt32BE } from './readInt32BE';
